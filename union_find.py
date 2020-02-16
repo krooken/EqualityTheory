41b85d2985d3f6a6c8feb5286d1cff26edac973b
Jonas Krook <jonas.krook@zenuity.com>
2020-02-16 15:49:38 +0100

    Simple implementation of union-find

---
+++ union_find.py
@@ -0,0 +1,35 @@
+class UnionNode:
+
+    def __init__(self, variable):
+
+        self.parent = None
+        self.children = []
+        self.variable = variable
+
+    def __eq__(self, other):
+        if self.parent is None and other.parent is None:
+            return self.variable == other.variable
+        else:
+            return self.find() == other.find()
+
+    def neighbors(self):
+        return self.children + [self.parent]
+
+    def find(self):
+
+        if self.parent is not None:
+
+            return self.parent
+
+        else:
+
+            return self
+
+    def merge(self, other):
+
+        self_root = self.find()
+        other_root = other.find()
+
+        if self_root != other_root:
+            other_root.parent = self_root
+            self_root.children.append(other_root)
